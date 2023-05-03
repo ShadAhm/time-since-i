105184c06c885b8761c7e800fdab1a025391e49d
Shad Ahmad <arsh90@gmail.com>
2023-05-03 08:49:25 +0200

    Added footer

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,16 @@ import CountupCards from './components/CountupCards/CountupCards';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Time Since I</h1>
-      <CountupCards></CountupCards>
-    </div>
+    <>
+      <div className="main-content">
+        <h1>Time Since I</h1>
+        <CountupCards></CountupCards>
+      </div>
+      <footer>
+        <p>Made by <a href="https://shadahm.github.io">Shad Ahm</a> for learning purposes only. Use at your own risk.</p>
+        <p>For code see the <a href="https://github.com/ShadAhm/time-since-i">Github repo</a>.</p>
+      </footer>
+    </> 
   );
 }
 
